c71393ea6fe025547365d1561cabbd137b0a419d
rifatrakib <Rakib.1508@outlook.com>
2023-08-29 09:15:02 +0600

    fix bug in cache operation function and take client as input

--- auth/server/database/cache/manager.py
+++ auth/server/database/cache/manager.py
@@ -1,18 +1,15 @@
-from server.security.dependencies.clients import get_redis_client
+from aioredis.client import Redis
 
 
-async def write_data_to_cache(key: str, data: str, expire: int = 3600) -> None:
-    redis = await get_redis_client()
-    await redis.set(key, data, ex=expire)
+async def write_data_to_cache(client: Redis, key: str, data: str, expire: int = 3600) -> None:
+    await client.set(key, data, ex=expire)
 
 
-async def read_from_cache(key: str) -> str:
-    redis = await get_redis_client()
-    return await redis.get(key)
+async def read_from_cache(client: Redis, key: str) -> str:
+    return await client.get(key)
 
 
-async def pop_from_cache(key: str) -> str:
-    redis = await get_redis_client()
-    data = await redis.get(key)
-    await redis.delete(key)
+async def pop_from_cache(client: Redis, key: str) -> str:
+    data = await client.get(key)
+    await client.delete(key)
     return data
--- auth/server/utils/helper.py
+++ auth/server/utils/helper.py
@@ -2,11 +2,12 @@ import json
 from typing import Any, Dict
 from uuid import uuid4
 
+from aioredis.client import Redis
 from pydantic import HttpUrl
 from server.database.cache.manager import write_data_to_cache
 
 
-def generate_temporary_url(data: Dict[str, Any], path: HttpUrl) -> HttpUrl:
+async def generate_temporary_url(client: Redis, data: Dict[str, Any], path: HttpUrl) -> HttpUrl:
     key = str(uuid4())
-    write_data_to_cache(key=key, data=json.dumps(data), expire=60)
+    await write_data_to_cache(client=client, key=key, data=json.dumps(data), expire=60)
     return f"{path}?key={key}"
