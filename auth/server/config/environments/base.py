078d34928a37ac7cd1003eda817291a197f7fe93
rifatrakib <Rakib.1508@outlook.com>
2023-08-30 10:06:14 +0600

    add functionalities to hash password with salt with double layers

--- auth/server/config/environments/base.py
+++ auth/server/config/environments/base.py
@@ -1,5 +1,7 @@
-from pydantic import Extra
-from pydantic_settings import BaseSettings
+from typing import Union
+
+from pydantic import EmailStr, Extra
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RootConfig(BaseSettings):
@@ -22,8 +24,32 @@ class BaseConfig(RootConfig):
     REDIS_HOST: str
     REDIS_PORT: int
 
-    class Config:
-        env_file = ".env"
+    # SMTP Configurations
+    MAIL_USERNAME: Union[EmailStr, str]
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool
+
+    # JWT Configurations
+    JWT_SECRET_KEY: str
+    JWT_SUBJECT: str
+    JWT_TOKEN_PREFIX: str
+    JWT_ALGORITHM: str
+    JWT_MIN: int
+    JWT_HOUR: int
+    JWT_DAY: int
+
+    # Hash Configurations
+    SALT_HASH_ALGORITHM: str
+    PASSWORD_HASH_ALGORITHM: str
+    HASH_SALT: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     @property
     def RDS_URI(self) -> str:
@@ -36,4 +62,4 @@ class BaseConfig(RootConfig):
 
     @property
     def REDIS_URI(self) -> str:
-        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/auth-cache"
