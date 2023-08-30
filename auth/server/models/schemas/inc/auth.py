36c872dbb9ececca57defade64db2cef51c0dd20
rifatrakib <Rakib.1508@outlook.com>
2023-08-30 08:30:52 +0600

    added and modified schemas related to authentication

--- auth/server/models/schemas/inc/auth.py
+++ auth/server/models/schemas/inc/auth.py
@@ -18,3 +18,4 @@ class SignupRequestSchema(BaseRequestSchema):
                 "Password must be between 8 and 64 characters long and contain at least one uppercase letter, one lowercase letter, one"
                 " digit and one special character."
             )
+        return v
