e64a3ade89157d156e39d277fea4dfc24b23d139
rifatrakib <Rakib.1508@outlook.com>
2023-08-31 10:39:59 +0600

    added functions to encrypt and decrypt JWT for authentication

---
+++ auth/server/security/authentication/jwt.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from typing import Union
+
+from jose import JWTError, jwt
+from pydantic import ValidationError
+from server.config.factory import settings
+from server.models.schemas.out.auth import TokenData, TokenUser
+
+
+def create_jwt(data: TokenUser, expires_delta: Union[datetime, None] = None) -> str:
+    expires_delta = expires_delta if expires_delta else timedelta(minutes=settings.JWT_MIN)
+    expire = datetime.utcnow() + expires_delta
+    to_encode = TokenData(**data.model_dump(), exp=expire, sub=settings.JWT_SUBJECT)
+    return jwt.encode(
+        to_encode.model_dump(),
+        key=settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM,
+    )
+
+
+def decode_jwt(token: str) -> TokenUser:
+    try:
+        payload = jwt.decode(
+            token=token,
+            key=settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM],
+        )
+        user_data = TokenUser(
+            id=payload.get("id"),
+            username=payload.get("username"),
+            email=payload.get("email"),
+            is_active=payload.get("is_active"),
+        )
+    except JWTError as token_decode_error:
+        raise ValueError("unable to decode JWT") from token_decode_error
+    except ValidationError as validation_error:
+        raise ValueError("invalid payload in JWT") from validation_error
+    return user_data
