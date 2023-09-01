1b38f973c8a2af006354f2a629cb3c9cd5ec08d7
rifatrakib <Rakib.1508@outlook.com>
2023-09-01 09:28:47 +0600

    add function to authenticate user with username and password

--- auth/server/database/account/crud.py
+++ auth/server/database/account/crud.py
@@ -1,7 +1,8 @@
-from fastapi import HTTPException, status
 from server.models.database.users import Account
 from server.models.schemas.inc.auth import SignupRequestSchema
 from server.security.authentication.password import pwd_generator
+from server.utils.exceptions import raise_401_unauthorized, raise_403_forbidden, raise_404_not_found, raise_409_conflict
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -23,7 +24,21 @@ async def create_user_account(session: AsyncSession, payload: SignupRequestSchem
 
         return new_account
     except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="username or email already exists",
-        )
+        raise_409_conflict(message="username or email already exists")
+
+
+async def authenticate_user(session: AsyncSession, username: str, password: str) -> Account:
+    stmt = select(Account).where(Account.username == username)
+    query = await session.execute(stmt)
+    user = query.scalar()
+
+    if not user:
+        raise_404_not_found(message=f"The username {username} is not registered.")
+
+    if not user.is_active:
+        raise_403_forbidden(message=f"The account for username {username} is not activated.")
+
+    if not pwd_generator.verify_password(user.hash_salt, password, user.hashed_password):
+        raise_401_unauthorized(message="Incorrect password.")
+
+    return user
