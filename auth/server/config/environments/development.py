ffe0cb68eb4968d3d3bc40822d617335878a3a63
rifatrakib <Rakib.1508@outlook.com>
2023-09-07 21:06:35 +0600

    add new settings mode to skip stmp mails

--- auth/server/config/environments/development.py
+++ auth/server/config/environments/development.py
@@ -4,3 +4,8 @@ from server.config.environments.base import BaseConfig
 class DevelopmentConfig(BaseConfig):
     DEBUG: bool = True
     MODE: str = "development"
+
+
+class IgnoreSMTPConfig(DevelopmentConfig):
+    DEBUG: bool = True
+    MODE: str = "ignore-smtp"
