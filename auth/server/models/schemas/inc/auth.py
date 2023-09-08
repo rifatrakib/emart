ba07a88a4caf59d6bb86cb92782e2fb1890e8538
rifatrakib <Rakib.1508@outlook.com>
2023-09-08 18:41:14 +0600

    remove unnecessary validation steps from backend internal schema

--- auth/server/models/schemas/inc/auth.py
+++ auth/server/models/schemas/inc/auth.py
@@ -1,24 +1,16 @@
-import re
-
-from pydantic import EmailStr, Field, validator
+from pydantic import EmailStr
 from server.models.schemas.base import BaseRequestSchema
-from server.models.schemas.base.fields import email_field, password_field, username_field
 
 
 class LoginRequestSchema(BaseRequestSchema):
-    username: str = Field(**username_field())
-    password: str = Field(**password_field())
+    username: str
+    password: str
 
 
 class SignupRequestSchema(LoginRequestSchema):
-    email: EmailStr = Field(**email_field())
+    email: EmailStr
 
-    @validator("password", pre=True)
-    def validate_password_pattern(cls, v) -> str:
-        pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,64}$"
-        if not re.match(pattern, v):
-            raise ValueError(
-                "Password must be between 8 and 64 characters long and contain at least one uppercase letter, one lowercase letter, one"
-                " digit and one special character."
-            )
-        return v
+
+class PasswordChangeRequestSchema(BaseRequestSchema):
+    current_password: str
+    new_password: str
