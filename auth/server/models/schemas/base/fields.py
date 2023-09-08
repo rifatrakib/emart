17357fe270a08984a592b01e22b83211060f5378
rifatrakib <Rakib.1508@outlook.com>
2023-09-08 19:55:27 +0600

    make password field attributes extendable for reuse, add examples

--- auth/server/models/schemas/base/fields.py
+++ auth/server/models/schemas/base/fields.py
@@ -11,6 +11,7 @@ def username_field() -> Dict[str, Any]:
         pattern=r"^[\w.@_-]{6,32}$",
         min_length=6,
         max_length=32,
+        example="superadmin",
     )
 
 
@@ -18,11 +19,12 @@ def email_field() -> Dict[str, Any]:
     return dict(
         title="email",
         decription="Unique email that can be used to identify users.",
+        example="admin@app.io",
     )
 
 
-def password_field() -> Dict[str, Any]:
-    return dict(
+def password_field(**kwargs) -> Dict[str, Any]:
+    field_attributes = dict(
         title="password",
         decription="""
             Password containing at least 1 uppercase letter, 1 lowercase letter,
@@ -31,4 +33,9 @@ def password_field() -> Dict[str, Any]:
         """.replace("\n", " ").strip(),
         min_length=8,
         max_length=64,
+        example="Admin@12345",
     )
+
+    if kwargs:
+        field_attributes.update(kwargs)
+    return field_attributes
