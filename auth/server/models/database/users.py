37712733e7129261c553bc26d74af44c6a395eeb
rifatrakib <Rakib.1508@outlook.com>
2023-09-16 07:49:54 +0600

    add necessary getter and setter methods in database models

--- auth/server/models/database/users.py
+++ auth/server/models/database/users.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import date
 
 from server.models.database import Base
-from sqlalchemy import Boolean, DateTime, ForeignKey, String
+from server.utils.enums import Gender
+from sqlalchemy import Boolean, Date, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -18,6 +19,9 @@ class Account(Base):
         back_populates="user_account",
         uselist=False,
     )
+
+    def __repr__(self) -> str:
+        return f"<Account(username={self.username}, email={self.email}>"
 
     @property
     def hashed_password(self) -> str:
@@ -38,8 +42,9 @@ class Profile(Base):
     first_name: Mapped[str] = mapped_column(String(length=64), nullable=False)
     middle_name: Mapped[str] = mapped_column(String(length=256), nullable=True)
     last_name: Mapped[str] = mapped_column(String(length=64), nullable=False)
-    birth_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
+    _birth_date: Mapped[date] = mapped_column(Date, nullable=True)
     address: Mapped[str] = mapped_column(String(length=1024), nullable=True)
+    _gender: Mapped[str] = mapped_column(String(length=1), nullable=True)
     account_id: Mapped[int] = mapped_column(ForeignKey("account.id", ondelete="CASCADE"), nullable=False)
 
     user_account: Mapped["Account"] = relationship(
@@ -48,3 +53,33 @@ class Profile(Base):
         back_populates="user_profile",
         single_parent=True,
     )
+
+    def __repr__(self) -> str:
+        return f"<Profile(first_name={self.first_name}, last_name={self.last_name}>"
+
+    @property
+    def full_name(self) -> str:
+        if self.middle_name:
+            return f"{self.first_name} {self.middle_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name_asian(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+    @property
+    def gender(self) -> Gender:
+        if self._gender == "m":
+            return Gender.male
+        elif self._gender == "f":
+            return Gender.female
+
+    def set_gender(self, gender: Gender) -> None:
+        self._gender = gender.value
+
+    @property
+    def birth_date(self) -> date:
+        return self._birth_date
+
+    def set_birth_date(self, birth_date: str) -> None:
+        self._birth_date = birth_date
