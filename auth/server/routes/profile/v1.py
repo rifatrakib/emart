c5bc214edf70c6dd8753fc829a75131e36d08bdf
rifatrakib <Rakib.1508@outlook.com>
2023-09-18 09:03:58 +0600

    add endpoint to read user data based on user profile

--- auth/server/routes/profile/v1.py
+++ auth/server/routes/profile/v1.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from server.database.profile.crud import create_user_profile
+from server.database.profile.crud import create_user_profile, read_profile_by_username
 from server.models.schemas.base import MessageResponseSchema
 from server.models.schemas.inc.profile import ProfileCreateSchema
 from server.models.schemas.out.auth import TokenUser
+from server.models.schemas.out.profile import ProfileResponse
 from server.security.dependencies.acl import authenticate_active_user
 from server.security.dependencies.clients import get_database_session
 from server.utils.enums import Tags, Versions
@@ -26,5 +27,22 @@ async def create_profile(
     try:
         new_profile = await create_user_profile(session, user.id, payload)
         return {"message": f"Profile created successfully for user {new_profile.user_account.username}."}
+    except HTTPException as e:
+        raise e
+
+
+@router.get(
+    "/@{username}",
+    summary="Get a user profile",
+    description="Get a user profile by username.",
+    response_model=ProfileResponse,
+)
+async def read_user_profile(
+    username: str,
+    session: AsyncSession = Depends(get_database_session),
+):
+    try:
+        user_profile = await read_profile_by_username(session, username)
+        return user_profile
     except HTTPException as e:
         raise e
