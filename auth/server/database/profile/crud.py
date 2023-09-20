e671f91a00980b3265afb0a71fe564ead7cad6f8
rifatrakib <Rakib.1508@outlook.com>
2023-09-20 22:13:13 +0600

    added database function to update profile data

--- auth/server/database/profile/crud.py
+++ auth/server/database/profile/crud.py
@@ -1,5 +1,5 @@
 from server.models.database.users import Account, Profile
-from server.models.schemas.inc.profile import ProfileCreateSchema
+from server.models.schemas.inc.profile import ProfileCreateSchema, ProfileUpdateSchema
 from server.utils.exceptions import raise_404_not_found, raise_409_conflict
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
@@ -40,3 +40,31 @@ async def read_profile_by_username(session: AsyncSession, username: str) -> Prof
         raise_404_not_found(message=f"No profile found for the user {username}.")
 
     return user_profile
+
+
+async def update_user_profile(
+    session: AsyncSession,
+    account_id: int,
+    payload: ProfileUpdateSchema,
+) -> Profile:
+    select_stmt = select(Profile).where(Profile.account_id == account_id)
+    query = await session.execute(statement=select_stmt)
+    update_user = query.scalar()
+
+    if not update_user:
+        raise_404_not_found(message=f"No profile found for the user with account id {account_id}.")
+
+    for field, update_value in payload.model_dump().items():
+        if update_value:
+            if field == "birth_date":
+                update_user.set_birth_date(birth_date=update_value)
+            if field == "gender":
+                update_user.set_gender(gender=update_value)
+            else:
+                setattr(update_user, field, update_value)
+
+    await session.flush()
+    await session.commit()
+    await session.refresh(instance=update_user)
+
+    return update_user
