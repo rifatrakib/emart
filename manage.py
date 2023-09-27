7751095ba9cef89c834dba1afb20b850778054fa
rifatrakib <Rakib.1508@outlook.com>
2023-09-27 11:11:35 +0600

    create separate env for postgres database config

--- manage.py
+++ manage.py
@@ -2,9 +2,18 @@ import subprocess
 from enum import Enum
 from typing import Union
 
+from pydantic import BaseModel
 from typer import Typer
 
 app = Typer()
+
+
+class PostgresConfig(BaseModel):
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
 
 class Modes(str, Enum):
@@ -22,6 +31,13 @@ def deploy(mode: Union[str, None] = "development"):
 
         with open("auth/.env", "w") as writer:
             writer.write(f"MODE={mode}")
+
+        with open(f"auth/secrets/{mode}.json") as reader:
+            secrets = PostgresConfig.model_validate_json(reader.read())
+
+        with open("auth/.env.postgres", "w") as writer:
+            for key, value in secrets.model_dump().items():
+                writer.write(f"{key}={value}\n")
 
         subprocess.run("docker compose up --build")
     except KeyError:
