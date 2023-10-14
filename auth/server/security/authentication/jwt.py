b96e4397f899a5838deaf64bfba1929aeaaddd80
rifatrakib <Rakib.1508@outlook.com>
2023-10-14 15:21:58 +0600

    store refresh token against access token as redis key

--- auth/server/security/authentication/jwt.py
+++ auth/server/security/authentication/jwt.py
@@ -7,10 +7,10 @@ from pydantic import ValidationError
 from server.config.factory import settings
 from server.database.cache.manager import write_data_to_cache
 from server.models.database.users import Account
-from server.models.schemas.out.auth import TokenData, TokenUser
+from server.models.schemas.out.auth import TokenCollectionSchema, TokenData, TokenUser
 
 
-def create_jwt(data: TokenUser, expires_delta: Union[datetime, None] = None) -> str:
+def create_access_token(data: TokenUser, expires_delta: Union[datetime, None] = None) -> str:
     expires_delta = expires_delta if expires_delta else timedelta(minutes=settings.JWT_MIN)
     expire = datetime.utcnow() + expires_delta
     to_encode = TokenData(**data.model_dump(), exp=expire, sub=settings.JWT_SUBJECT)
@@ -21,7 +21,18 @@ def create_jwt(data: TokenUser, expires_delta: Union[datetime, None] = None) -> 
     )
 
 
-def decode_jwt(token: str) -> TokenUser:
+def create_refresh_token(data: TokenUser, expires_delta: Union[datetime, None] = None) -> str:
+    expires_delta = expires_delta if expires_delta else timedelta(minutes=settings.REFRESH_TOKEN_MIN)
+    expire = datetime.utcnow() + expires_delta
+    to_encode = TokenData(**data.model_dump(), exp=expire, sub=settings.REFRESH_TOKEN_SUBJECT)
+    return jwt.encode(
+        to_encode.model_dump(),
+        key=settings.REFRESH_TOKEN_SECRET_KEY,
+        algorithm=settings.REFRESH_TOKEN_ALGORITHM,
+    )
+
+
+def decode_access_token(token: str) -> TokenUser:
     try:
         payload = jwt.decode(
             token=token,
@@ -43,7 +54,7 @@ def decode_jwt(token: str) -> TokenUser:
     return user_data
 
 
-async def get_jwt(redis: Redis, user: Account):
+async def get_jwt(redis: Redis, user: Account) -> TokenCollectionSchema:
     token_data = TokenUser(
         id=user.id,
         username=user.username,
@@ -52,12 +63,13 @@ async def get_jwt(redis: Redis, user: Account):
         open_id=user.open_id,
         provider=user.provider,
     )
-    token = create_jwt(token_data)
+    access_token = create_access_token(token_data)
+    refresh_token = create_refresh_token(token_data)
 
     await write_data_to_cache(
         redis,
-        token,
-        token_data.model_dump_json(),
+        access_token,
+        refresh_token,
         settings.JWT_MIN * 60,
     )
-    return token
+    return TokenCollectionSchema(access_token=access_token, refresh_token=refresh_token)
