e7cdc051cabb4e6b8b2f21eb091131bb68861fff
rifatrakib <Rakib.1508@outlook.com>
2023-10-16 10:37:56 +0600

    add decoding methods in jwt generator class

--- auth/server/security/authentication/token.py
+++ auth/server/security/authentication/token.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timedelta
+from typing import Any, Dict
 
-from jose import jwt
+from aioredis.client import Redis
+from jose import JWTError, jwt
+from pydantic import ValidationError
 from server.config.factory import settings
-from server.models.schemas.out.auth import TokenUser
+from server.database.cache.manager import write_data_to_cache
+from server.models.database.users import Account
+from server.models.schemas.out.auth import TokenCollectionSchema, TokenUser
 from server.utils.enums import TimeUnits
 
 
@@ -54,4 +59,59 @@ class JWTGenerator:
             },
             key=self.refresh_token_secret_key,
             algorithm=self.refresh_token_algorithm,
+        )
+
+    def identify_user(self, payload: Dict[str, Any]) -> TokenUser:
+        return TokenUser(
+            id=payload.get("id"),
+            username=payload.get("username"),
+            email=payload.get("email"),
+            is_active=payload.get("is_active"),
+            open_id=payload.get("open_id"),
+            provider=payload.get("provider"),
+        )
+
+    def decode_access_token(self, token: str) -> TokenUser:
+        try:
+            payload = jwt.decode(
+                token=token,
+                key=self.access_token_secret_key,
+                algorithms=[self.access_token_algorithm],
+            )
+            return self.identify_user(payload)
+        except JWTError:
+            raise ValueError("unable to decode JWT")
+        except ValidationError:
+            raise ValueError("invalid payload in JWT")
+
+    def decode_refresh_token(self, token: str) -> TokenUser:
+        try:
+            payload = jwt.decode(
+                token=token,
+                key=self.refresh_token_secret_key,
+                algorithms=[self.refresh_token_algorithm],
+            )
+            return self.identify_user(payload)
+        except JWTError:
+            raise ValueError("unable to decode JWT")
+        except ValidationError:
+            raise ValueError("invalid payload in JWT")
+
+    async def generate_jwt(self, redis: Redis, user: Account) -> TokenCollectionSchema:
+        token_data = TokenUser(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+            is_active=user.is_active,
+            open_id=user.open_id,
+            provider=user.provider,
+        )
+
+        access_token = self.create_access_token(token_data)
+        refresh_token = self.create_refresh_token(token_data)
+        await write_data_to_cache(redis, access_token, refresh_token)
+
+        return TokenCollectionSchema(
+            access_token=access_token,
+            refresh_token=refresh_token,
         )
