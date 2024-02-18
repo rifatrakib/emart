c1edbf13e2c7c1de336907ee891c3d9dbe8698e2
rifatrakib <Rakib.1508@outlook.com>
2024-02-18 20:22:42 +0600

    update app create schema and add app response schema

--- gateway/server/models/schemas/requests/applications.py
+++ gateway/server/models/schemas/requests/applications.py
@@ -1,6 +1,9 @@
+from typing import Union
+
 from server.models.schemas.requests import BaseRequestSchema
 
 
 class ApplicationCreateSchema(BaseRequestSchema):
     name: str
+    description: Union[str, None] = None
     callback_url: str
---
+++ gateway/server/models/schemas/responses/applications.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+from server.models.schemas.responses import BaseResponseSchema
+
+
+class ApplicationResponse(BaseResponseSchema):
+    name: str
+    description: Union[str, None] = None
+    callback_url: str
+    client_id: str
+    secret_key: str
