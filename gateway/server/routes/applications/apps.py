b31caf1daded3c22d787938dcd03841e53639578
rifatrakib <Rakib.1508@outlook.com>
2024-02-22 09:32:31 +0600

    added new endpoint to read all applications created by logged in user

--- gateway/server/routes/applications/apps.py
+++ gateway/server/routes/applications/apps.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.connections.clients import get_database_session
 from server.database.applications.create import create_new_application
+from server.database.applications.read import read_applications
 from server.models.schemas.requests.applications import ApplicationCreateSchema
 from server.models.schemas.responses import MessageResponseSchema
 from server.models.schemas.responses.applications import ApplicationResponse
@@ -27,6 +30,17 @@ def create_oauth_applications_router() -> APIRouter:
     ) -> ApplicationResponse:
         try:
             app = await create_new_application(session, payload, user)
+            return app
+        except Exception as e:
+            raise e
+
+    @router.get("", response_model=List[ApplicationResponse])
+    async def read_oauth_application(
+        user: TokenUser = Depends(authenticate_active_user),
+        session: AsyncSession = Depends(get_database_session),
+    ) -> ApplicationResponse:
+        try:
+            app = await read_applications(session, user.id)
             return app
         except Exception as e:
             raise e
