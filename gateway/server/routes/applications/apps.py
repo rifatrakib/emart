aec7cef0db645ae74b71027caa505990a5eedc0b
rifatrakib <Rakib.1508@outlook.com>
2024-02-25 22:22:39 +0600

    added endpoint to update oauth app info

--- gateway/server/routes/applications/apps.py
+++ gateway/server/routes/applications/apps.py
@@ -6,12 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from server.connections.clients import get_database_session
 from server.database.applications.create import create_new_application
 from server.database.applications.read import read_applications
-from server.models.schemas.requests.applications import ApplicationCreateSchema
+from server.database.applications.update import update_application
+from server.models.schemas.requests.applications import ApplicationCreateSchema, ApplicationUpdateSchema
 from server.models.schemas.responses import MessageResponseSchema
 from server.models.schemas.responses.applications import ApplicationResponse
 from server.models.schemas.responses.auth import TokenUser
 from server.security.dependencies.acl import authenticate_active_user
 from server.utils.enums import Tags
+from server.utils.exceptions import handle_422_unprocessable_entity
 from server.utils.helpers import create_tags
 
 
@@ -41,6 +43,21 @@ def create_oauth_applications_router() -> APIRouter:
     ) -> ApplicationResponse:
         try:
             app = await read_applications(session, user.id)
+            return app
+        except Exception as e:
+            raise e
+
+    @router.patch("/{app_id}", response_model=ApplicationResponse)
+    async def update_oauth_application(
+        app_id: int,
+        payload: ApplicationUpdateSchema,
+        user: TokenUser = Depends(authenticate_active_user),
+        session: AsyncSession = Depends(get_database_session),
+    ) -> ApplicationResponse:
+        try:
+            if not payload.model_dump(exclude_unset=True):
+                raise handle_422_unprocessable_entity("No data to update.")
+            app = await update_application(session, app_id, user.id, payload)
             return app
         except Exception as e:
             raise e
